7ceaec2fa090cd1942c81a04d5f075713131e617
Abrentis <brentknox@rocketmail.com>
2023-08-10 19:52:14 -0400

    Added JS code to display book title when given an author's name

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,8 @@ var searchURLq = "https://openlibrary.org/search.json?q=";
 var searchURLAuthor = "https://openlibrary.org/search.json?author=";
 var searchURLTitle = "https://openlibrary.org/search.json?title=";
 localStorage.clear();
+
+var displayListEl = $("#display-list");
 var authorEl = document.getElementById("author-name");
 var isbnEl = document.getElementById("isbn");
 var submitEl = document.getElementById('submit');
@@ -35,51 +37,6 @@ function search(event) {
 }
 searchBar.on("submit", search);
 
-
-function initDropdowns() {
-  var $document = $(document);
-
-  /**
-   * hide dropdown menu if user clicks away from dropdown
-  */
-  $document.on('click', function () {
-    $(".dropdown.is-active").removeClass("is-active");
-  });
-  
-  /**
-   * show and hide dropdown menu on clicked button
-   */
-  $document.on('click', ".dropdown .button", function (event) {
-    event.stopPropagation();
-    $(event.target).closest(".dropdown").toggleClass('is-active');
-  });
-}
-
-/**
- * jQuery ready wrapper waits for the document to emit the DOMContentLoaded event.
- * This ensures your html exists on the DOM before querying for your elements.
- * Also prevents your variables from being accessed in the global scope.
- * 
- * All of your code could live inside that ready callback function.
- */
-$(document).ready(function () {
-  var $dropdownItems = $(".dropdown-item");
-
-  initDropdowns();
-
-
-  /**
-   * change active dropdown item
-   * removes the 'is-active' class from any active sibling element
-   * adds 'is-active' to the clicked dropdown item
-   */
-  $dropdownItems.on("click", function (event) {
-    console.log(event.target);
-    var $item = $(event.target);
-    $item.siblings().removeClass("is-active");
-    $item.addClass("is-active");
-  });
-})
 url = "";
 var authorEl = document.getElementById("author-name");
 var isbnEl = document.getElementById("isbn");
@@ -153,6 +110,7 @@ function isbn_det(){
 //"OL23919A", Toni Morrison, JK Rowling, Sam Harris, Richard Dawkins
 
     function getWork (workID) {
+    displayListEl.empty();
     var workURL = `https://openlibrary.org/authors/${workID}/works.json?limit=10`
     var ratingURL = `https://openlibrary.org/works/${workID}/ratings.json`
     fetch(workURL)
@@ -164,7 +122,8 @@ function isbn_det(){
     .then(function (data) {
       console.log(data); 
       //console.log(data.entries.length);
-      for (var i=0; i<data.entries.length; i=i+1) {
+      for (var i=0; i<data.entries.length; i++) {
+        displayListEl.append("<li>" + data.entries[i].title + "</li>")
         console.log(data.entries[i].title);
       }
       
@@ -182,4 +141,3 @@ function isbn_det(){
     
     
   }
-
